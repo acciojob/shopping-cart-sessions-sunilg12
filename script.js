944cd4681c08b053c3be7c74c16676b1d275a2d5
Sunil Reddy Gaddam <gaddamsunilreddy20@gmail.com>
2025-12-15 20:49:38 +0530

    created a submission

--- script.js
+++ script.js
@@ -12,27 +12,70 @@ const products = [
 // DOM elements
 const productList = document.getElementById("product-list");
 
+// let productsAdded=document.createElement("ul");
+
 // Render product list
 function renderProducts() {
   products.forEach((product) => {
     const li = document.createElement("li");
-    li.innerHTML = `${product.name} - $${product.price} <button class="add-to-cart-btn" data-id="${product.id}">Add to Cart</button>`;
+    li.innerHTML = `${product.name} - $${product.price} <button onClick="addToCart(${product.id})">Add to Cart</button>`;
     productList.appendChild(li);
   });
 }
 
 // Render cart list
-function renderCart() {}
+function renderCart() {
+	let cartDiv = document.getElementById("cart-list");
+	cartDiv.innerHTML="";
+
+	let ul = document.createElement("ul");
+	let total=0;
+
+	for(let i=0; i<cart.length; i++){
+		let item=cart[i];
+
+		let cartList = document.createElement("li");
+		cartList.innerHTML=`${item.name} - $${item.price}`;
+
+		ul.appendChild(cartList);
+		total += item.price;
+	}
+
+	cartDiv.appendChild(ul);
+	let totalPrice=document.createElement("h3");
+	totalPrice.innerHTML = `Total= $${total}`;
+	cartDiv.appendChild(totalPrice);
+}
 
 // Add item to cart
-function addToCart(productId) {}
+let cart=[];
+function addToCart(productId) {
+	let product=products.find(p => (p.id === productId));
+
+	if(product){
+		cart.push(product);
+		renderCart();
+	}
+}
 
 // Remove item from cart
-function removeFromCart(productId) {}
+function removeFromCart(productId) {
+	const index = cart.findindex(item => item.id === productId);
+	if(index !== -1){
+	cart.splice(index,1);
+	renderCart();
+	}
+}
 
 // Clear cart
-function clearCart() {}
+function clearCart() {
+	cart.length=0;
+	renderCart();
+}
 
 // Initial render
 renderProducts();
 renderCart();
+
+let clickClear = document.getElementById("clear-cart-btn");
+clickClear.addEventListener("click" ,clearCart);
